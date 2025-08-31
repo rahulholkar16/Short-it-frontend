1789b12a49f7c9945544083bc5b01c38c786d7b5
Rahul Pal <usefulltech1819@gmail.com>
2025-08-31 13:34:00 +0530

    add history page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer/Footer'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import Dashboard from './pages/Dashboard/Dashboard'
+import History from './pages/History/History'
 function App() {
 
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/login' element={ <Login /> } />
         <Route path='/signup' element={ <Signup /> } />
         <Route path='/dashboard' element={ <Dashboard /> } />
+        <Route path='/history' element={ <History /> } />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/History/History.jsx
@@ -0,0 +1,17 @@
+import './History.css'
+
+export default function History() {
+    return (
+        <main className="container">
+            <div className="history-card">
+                <h2>Your Link History</h2>
+
+                <div className="table-wrapper">
+
+
+                </div>
+            </div>
+        </main>
+
+    )
+}
