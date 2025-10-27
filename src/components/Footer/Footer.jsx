74e316bb137bcd1819aee51aad2f9b8fce533b1a
Rahul Pal <116374045+rahulholkar16@users.noreply.github.com>
2025-10-27 19:33:42 +0530

    Add 'Made By Rahul' to footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 export default function Footer() {
   return (
       <footer>
-          <p>© 2025 ShortIt | Built with ❤️ using Node.js & MongoDB</p>
+          <p>© 2025 ShortIt | Built with ❤️ using Node.js & MongoDB | Made By Rahul</p>
       </footer>
   )
 }
