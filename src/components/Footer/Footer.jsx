d48f3888f722d3692e8c121c9844136b38304784
Rahul Pal <116374045+rahulholkar16@users.noreply.github.com>
2025-11-09 19:30:51 +0530

    Update footer with new author link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 export default function Footer() {
   return (
       <footer>
-      <p>© 2025 ShortIt | Built with ❤️ using Node.js & MongoDB | Made by <a href='https://shortit-tan.vercel.app/'>UNKNOWN</a></p>
+      <p>© 2025 ShortIt | Built with ❤️ using Node.js & MongoDB | Made by <a href='https://www.linkedin.com/in/rahulpal01/'>Rahul</a></p>
       </footer>
   )
 }
